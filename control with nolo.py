cf5c2c591fae04e33013f5f0e8a5be4fa8331865
112233zzxxll <1370589797@qq.com>
2026-01-14 18:35:54 +0800

    none

--- control with nolo.py
+++ control with nolo.py
@@ -22,27 +22,31 @@ ee_pos, ee_rot, target = env.reset_ee()
 # ----------------- 初始化求解模型 ----------------
 configuration, tasks, end_effector_task, solver, limits, model0, data0 = K.init_model() # 所有求解中间变量打包
 
-# # --------------- 相机预设置 ---------------
-# cam1_id = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_CAMERA, "rgb_camera")
-# cam2_id = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_CAMERA, "fixed_camera")
-# renderer = mujoco.Renderer(model, height=224, width=224)
+# --------------- 相机预设置 ---------------
+cam1_id = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_CAMERA, "rgb_camera")
+cam2_id = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_CAMERA, "fixed_camera")
+renderer = mujoco.Renderer(model, height=224, width=224)
 
 # --------------- 仿真 ---------------
 with mujoco.viewer.launch_passive(model, data) as viewer:
+    step = 0
     while viewer.is_running():
 
-        # # --------------- 相机设置模块 ---------------
-        # renderer.update_scene(data, camera=cam1_id)
-        # img1 = renderer.render()
-        # renderer.update_scene(data, camera=cam2_id)
-        # img2 = renderer.render()
-        # # 显示离屏图像
-        # cv2.imshow("Top View", cv2.cvtColor(img1, cv2.COLOR_RGB2BGR))
-        # cv2.imshow("Side View", cv2.cvtColor(img2, cv2.COLOR_RGB2BGR))
-        # cv2.waitKey(1)
+        # --------------- 相机设置模块 ---------------
+        if step % 100 == 0:  # 每100步采一帧
+            renderer.update_scene(data, camera=cam1_id)
+            img1 = renderer.render()
+            renderer.update_scene(data, camera=cam2_id)
+            img2 = renderer.render()
+            # 显示离屏图像
+            cv2.imshow("Top View", cv2.cvtColor(img1, cv2.COLOR_RGB2BGR))
+            cv2.imshow("Side View", cv2.cvtColor(img2, cv2.COLOR_RGB2BGR))
+            cv2.waitKey(1)
 
         d_pos, d_so3, button = nolo_tracker.get_delta() # 获取相对位移
         if button > 0:
+            step += 1
+            print(step)
             ee_pos, ee_rot, target = K.get_target(ee_pos, ee_rot, d_pos, d_so3) # 末端位姿
             model.body("target").pos = ee_pos  # 立即生效
             quat = ee_rot.parameters()
@@ -52,10 +56,11 @@ with mujoco.viewer.launch_passive(model, data) as viewer:
             else:
                 data.ctrl[7] = 0
             if button > 1000: # 环境重置
-                time.sleep(0.3)
+                time.sleep(0.5)
                 env.reset_target()
                 ee_pos, ee_rot, target = env.reset_ee()
                 data.ctrl[7] = 0
+                step = 0
         result = K.solve(configuration, tasks, end_effector_task, solver, limits, model0, data0, target)
         data.ctrl[:7] = result
         mujoco.mj_step(model, data)
