2c77b03522429597dac546dc8d915137690f1267
112233zzxxll <1370589797@qq.com>
2026-01-22 00:38:49 +0800

    build ppo

--- ppo_agent.py
+++ ppo_agent.py
@@ -1,2 +1,92 @@
 import torch
+import torch.nn as nn
+from torch.distributions import Normal # 正态分布采样
+import torch.nn.functional as F # 四元数归一化
+import torch.optim as optim
+import numpy as np
 
+class CNN(nn.Module): # 图像卷积，不参与参数更新，卷积前记得进行图像预处理，要求输入112*112的img
+    def __init__(self):
+        super().__init__()
+        # 共享的图片处理层 输入为 112 * 112 这里只处理了一个图像，输出为256
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=5, stride=1, padding=2), # 32 112 112 
+            nn.ReLU(),
+            nn.MaxPool2d(2), # 32 56 56
+            nn.Conv2d(32, 64, kernel_size=5, stride=1, padding=2), # 64 56 56
+            nn.ReLU(),
+            nn.AdaptiveAvgPool2d((4, 4)), # 64 4 4
+            nn.Flatten() # 展平
+        )
+        self.fc = nn.Linear(64*4*4, 256)
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.fc(x)
+        return x
+
+class ActorCritic(nn.Module):
+    def __init__(self, state_dim, action_dim, hidden_dim): # hidden_dim = 32
+        super().__init__()
+        # Actor 网络
+        # 输出动作的正态分布，数据结构为[mu1,mu2,...,mu7,sigma1, sigma2,...,sigma7]
+        # 前三个是dx,dy,dz,后四个是d_so3四元数
+        self.Actor = nn.Sequential(
+            nn.Linear(state_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, 2*action_dim), 
+        )
+
+        # Critic网络
+        # 增加隐藏层，增强数据表达能力
+        self.Critic = nn.Sequential(
+            nn.Linear(state_dim, 4*hidden_dim),
+            nn.ReLU(),
+            nn.Linear(4*hidden_dim, 4*hidden_dim),
+            nn.ReLU(),
+            nn.Linear(4*hidden_dim, 4*hidden_dim),
+            nn.ReLU(),
+            nn.Linear(4*hidden_dim, 1), 
+        )
+
+    def forward(self, x):
+        action_probs = self.Actor(x)
+        state_value = self.Critic(x)
+        return action_probs, state_value
+    
+class PPO:
+    def __init__(self, 
+                 state_dim, 
+                 action_dim, 
+                 lr=1e-4, 
+                 ):
+        self.state_dim = state_dim
+        self.action_dim = action_dim
+        self.lr = lr
+
+        self.policy = ActorCritic(state_dim, action_dim)
+        self.optimaizer = optim.Adam(self.policy.parameters(), lr=lr)
+
+        self.old_policy = ActorCritic(state_dim, action_dim)
+        self.old_policy.load_state_dict(self.policy.state_dict()) # 复制策略
+
+    def select_action(self, state):
+        with torch.no_grad(): # 跳过梯度计算，防止反向传播
+            # 注意这里的一些修改！！！！可能会报错
+            action_probs, state_value = self.old_policy(state)
+
+        # 重构输出的action_probs
+        # 先按照概率采样，然后把四元数项归一化
+        mu = torch.tensor(action_probs[0: 7])
+        sigma = torch.tensor(action_probs[7: 14])
+        dist = Normal(loc=mu, scale=sigma)
+        action = dist.sample()
+        log_prob = dist.log_prob(action)
+        action[3:7] = F.normalize(action[3:7], p=2, dim=-1)
+
+        return action.numpy(), log_prob.numpy(), state_value.numpy()
+    
--- test.py
+++ test.py
@@ -1,9 +1,24 @@
 import numpy as np
+import torch
+from torch.distributions import Normal
+import torch.nn.functional as F # 四元数归一化
 
-A = [[0,0,0], [1,1,1]]
-B = [[2,2,2], [3,3,3]]
-A = np.array(A)
-B = np.array(B)
-
-result = np.sum((A[0] - B[0]) ** 2)
-print(result)
+action_probs = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0]
+mu = torch.tensor(action_probs[0: 7])
+sigma = torch.tensor(action_probs[7: 14])
+# print(mu)
+# print(sigma)
+dist = Normal(loc=mu, scale=sigma)
+sample = dist.sample()
+log_prob = dist.log_prob(sample)
+# print(dist, sample, log_prob)
+x_np = sample.numpy()
+sum1 = np.sum(x_np[3:7])
+# print(sum1)
+sample[3:7] = F.normalize(sample[3:7], p=2, dim=-1)
+print(sample)
+action = sample.numpy()
+a=0
+for i in range(3,7):
+    a+=action[i]**2
+    print(a)
