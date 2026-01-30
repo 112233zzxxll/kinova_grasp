24179d0b5d8371b28506d45e3e3a21fe73af2f0e
112233zzxxll <1370589797@qq.com>
2026-01-30 16:01:49 +0800

    train ppo

--- ppo_agent.py
+++ ppo_agent.py
@@ -46,13 +46,13 @@ class ActorCritic(nn.Module):
         # Critic网络
         # 增加隐藏层，增强数据表达能力
         self.Critic = nn.Sequential(
-            nn.Linear(state_dim, 4*hidden_dim),
+            nn.Linear(state_dim, 2*hidden_dim),
             nn.ReLU(),
-            nn.Linear(4*hidden_dim, 4*hidden_dim),
+            nn.Linear(2*hidden_dim, 2*hidden_dim),
             nn.ReLU(),
-            nn.Linear(4*hidden_dim, 4*hidden_dim),
+            nn.Linear(2*hidden_dim, 2*hidden_dim),
             nn.ReLU(),
-            nn.Linear(4*hidden_dim, 1), 
+            nn.Linear(2*hidden_dim, 1), 
         )
 
     def forward(self, x):
@@ -103,93 +103,64 @@ class PPO:
         self.optimizer_actor = optim.Adam(self.policy.Actor.parameters(), lr=lr) # Actor 更新器
         self.optimizer_critic = optim.Adam(self.policy.Critic.parameters(), lr=lr) # Critic 更新器
 
-        self.old_policy = ActorCritic(self.state_dim, self.action_dim, self.hidden_dim)
-        self.old_policy.load_state_dict(self.policy.state_dict()) # 复制策略
-
-    def select_action(self, state):
+    def select_action(self, state): # 输入 numpy 数组
+        state = torch.tensor(state, dtype=torch.float32) # 先转 torch 张量
         with torch.no_grad(): # 跳过梯度计算，防止反向传播
             # 注意这里的一些修改！！！！可能会报错
-            action_probs, state_value = self.old_policy(state) # 旧策略采样
-
-        # 重构输出的action_probs
-        # 先按照概率采样，然后把四元数项归一化
+            action_probs, state_value = self.policy(state) # 策略采样
+        # 按照概率采样，（把四元数项归一化）
         mu = action_probs[0: self.action_dim]
         sigma = action_probs[self.action_dim: 2*self.action_dim]
         dist = Normal(loc=mu, scale=sigma)
         action = dist.sample()
         log_prob = dist.log_prob(action).sum()
         # action[4:8] = F.normalize(action[4:8], p=2, dim=-1) # 四元数归一化
-
-        return action.cpu().numpy(), log_prob.item(), state_value.cpu().item() 
+        return action.numpy(), log_prob.item(), state_value.item() 
         # 根据观测，返回采样到的动作、对应的动作概率密度对数总和、状态价值
 
-    def compute_gae(self, rewards, values): # 针对一条rollout
-        # 通过values获取nevt values
-        next_values = np.zeros_like(values)
-        for i in range(len(next_values)):
-            if i == len(next_values) - 1:
-                next_values[i] = 0
-            else:
-                next_values[i] = values[i+1]
-    
-        # 计算TD误差
-        TD_errors = []
-        for t in range(len(rewards)):
-            delta = rewards[t] + self.gamma * next_values[t] - values[t]
-            TD_errors.append(delta)
-        
-        # 计算GAE优势
+    def compute_gae(self, rewards, values, next_values, dones):
+        # 针对一次 rollout 数据进行计算
+        # 输入 numpy 数组
         advantages = []
         advantage = 0
-        for l in reversed(range(len(TD_errors))):
-            advantage = TD_errors[l] + self.gamma * self.gae_lambda * advantage
-            advantages.insert(0, advantage)
-
-        # 计算回报
         returns = []
-        for r in range(len(rewards)):
-            critic_return = advantages[r] + values[r]
-            returns.append(critic_return)
-
+        for i in reversed(range(len(dones))):
+            TD_erro = rewards[i] + self.gamma * next_values[i] - values[i]
+            advantage = TD_erro + self.gamma * self.gae_lambda * advantage * (1-dones[i])
+            c_return = advantage + values[i]
+            advantages.insert(0, advantage)
+            returns.insert(0, c_return)
         return advantages, returns
             
-    def update(self, advantages, n_batchs, n_epoch, old_log_probs, states, actions, returns):
-        # 先划分batch
-        # 共 10 个 episode 的 rollout，每个 episode 包含 1000 步，总共 1000*10 组的参数随机打乱
-        # 划分8个 batch，每个 batch 包含1250 个数据
-        # n_epoch 是每组数据使用 n 次
+    def update(self, n_epoch, n_batch, advantages, returns, old_log_probs, states, actions):
+        # n_epoch 是数据集被遍历的次数
+        # n_batch 是每次遍历时分包个数
+        # old_log_probs, states, actions用于重要性采样
+        advantages = torch.tensor(np.array(advantages), dtype=torch.float32)
+        old_log_probs = torch.tensor(np.array(old_log_probs), dtype=torch.float32)
+        states = torch.tensor(np.array(states), dtype=torch.float32)
+        actions = torch.tensor(np.array(actions), dtype=torch.long)
+        returns = torch.tensor(np.array(returns), dtype=torch.float32)
+        batch_size = len(advantages)//n_batch
+        # advantages = (advantages - advantages.mean()) / (advantages.std() + 1e-8) # 标准化
 
-
-        # 转为张量形式
-        advantages = torch.tensor(advantages, dtype=torch.float32)
-        # old_log_probs = torch.tensor(old_log_probs, dtype=torch.float32)
-        # states = torch.tensor(states, dtype=torch.float32)
-        # actions  = torch.tensor(actions, dtype=torch.float32)
-        returns = torch.tensor(returns, dtype=torch.float32)
-
-        batch_size = len(advantages)//n_batchs
-        for epoch in range(n_epoch): # 训练轮数
-
+        for epoch in range(n_epoch): 
+            # 首先划分 batch
             T = len(advantages)
-            indices = torch.randperm(T) # 索引随机打乱
-            for i in range(0, len(advantages), batch_size):
-                idx = indices[i: i+batch_size] # 随机索引
-                # 针对每个 batch 进行如下操作
-                # 重要性重采样
+            indices = torch.randperm(T)
+            for i in range(0, T, batch_size):
+                idx = indices[i: i+batch_size]
+
+                # 在每个 batch 下进行重要性采样
                 new_action_probs, new_state_value = self.policy(states[idx])
-                mu = new_action_probs[:, :self.action_dim]
-                sigma = F.softplus(new_action_probs[:, self.action_dim:]) + 1e-6
-                dist = Normal(loc=mu, scale=sigma)
-                new_log_probs = dist.log_prob(actions[idx]).sum(dim=1)
-
+                dist = torch.distributions.Categorical(new_action_probs)
+                new_log_probs = dist.log_prob(actions[idx])
                 # 计算概率比率
-                ratios = torch.exp(new_log_probs - old_log_probs[idx])
-
+                ratios = torch.exp(new_log_probs.sum(dim=1) - old_log_probs[idx])
                 # Actor损失裁剪
                 loss1 = ratios * advantages[idx]
                 loss2 = torch.clamp(ratios, 1 - self.epsilon, 1 + self.epsilon) * advantages[idx]
                 Actor_loss = -torch.min(loss1, loss2).mean()
-
                 # 更新 Actor
                 self.optimizer_actor.zero_grad()
                 Actor_loss.backward()
@@ -199,6 +170,8 @@ class PPO:
 
                 # 更新 Critic
                 critic_loss = nn.MSELoss()(new_state_value.squeeze(-1), returns[idx])
+                # print(critic_loss)
+                # print(new_state_value.squeeze(),"|||",  returns[idx])
                 self.optimizer_critic.zero_grad()
                 critic_loss.backward()
                 torch.nn.utils.clip_grad_norm_(self.policy.Critic.parameters(), 0.5)
